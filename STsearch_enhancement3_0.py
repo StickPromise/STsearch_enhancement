49211222aed690ab647ceef2aa9a43bf89bf3578
Zhichen <102360493+StickPromise@users.noreply.github.com>
2023-08-16 18:05:59 +0800

    Update STsearch_enhancement3_0.py

--- STsearch_enhancement3_0.py
+++ STsearch_enhancement3_0.py
@@ -47,13 +47,13 @@ def process_model_output(response):
     contents = []
     lines = response.split('\n')
     field_to_id = {
-        "文献名": "2",
+        "书籍全名": "2",
         "文献来源": "6",
         "作者": "3",
-        "近代期刊-出版地": "124",
-        "近代图书-出版时间": "126",
-        "近代图书-出版者": "125",
-        "近代图书-主题词": "141"
+        "地点": "124",
+        "出版时间": "126",
+        "出版者": "125",
+        "主题词": "141"
     }
 
     for line in lines:
@@ -86,8 +86,8 @@ def get_ans(prompt):
 # 告诉模型有什么字段和含义，获得加工后的prompt
 def process_query(query):
     # 提供上下文信息
-    example = "作者：鲁迅\n近代图书-出版时间：[1930 TO 1940]\n近代图书-出版地：北平\n近代图书-主题词：社会文学\n"
-    field = "（文献名）、（文献来源）、（作者）、（近代图书-出版时间）、（近代图书-出版地）、（近代图书-出版者）、（近代图书-主题词）"
+    example = "作者：鲁迅\n出版时间：[1930 TO 1940]\n地点：北平\n主题词：社会文学\n"
+    field = "（书籍全名）、（文献来源）、（作者）、（出版时间）、（地点）、（出版者）、（主题词）"
     example3 = "AND,\nOR"
     rest = '并输出语句中的查询字段间的逻辑关系（AND、OR、NOT），只按顺序返回给我这几个查询字段的逻辑关系即可，如：{example3}, 逻辑关系的数量是查询字段的数量 - 1'
     prompt = f"我们有以下查询字段：\n{field}。 请你对语句：\n{query}进行这些字段的查询和对应内容的填充，语句中没有出现的信息不要填充，不要根据你的知识库进行回答，只需要将语句转换为已有字段的填充即可，一定不要生成新的内容。回答的格式按照下面的即可：字段名：内容\n字段名：内容\n。下面是一个例子：\n query:请找出鲁迅在1930年到1940年在北平写的有关社会文学的作品。\n你的回答应该是：{example}。"
@@ -129,10 +129,10 @@ def create_json(fields, contents):
         for field, content in zip(fields, contents):
             content = content.strip()
             content = convert_decade(content)  # 转换时间字段
-            if content and content.lower() != ("无", "NULL", "不填充"):
+            if content and content.lower() not in ("无", "NULL", "null", "不填充"):
                 filtered_fields.append(field.strip())
                 filtered_contents.append(content)
-        combines = ["AND"] * (len(filtered_fields) - 1)
+        combines = ["OR"] * (len(filtered_fields) - 1)
         types = ["0"] * len(filtered_fields)
         result = {
             "fields": filtered_fields,
