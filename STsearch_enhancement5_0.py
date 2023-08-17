0903c004f61df9523ec4e49af64f59c9de42bf55
Zhichen <102360493+StickPromise@users.noreply.github.com>
2023-08-17 18:17:53 +0800

    Update and rename STsearch_enhancement3_0.py to STsearch_enhancement5_0.py

--- STsearch_enhancement5_0.py
+++ STsearch_enhancement5_0.py
@@ -6,7 +6,7 @@ import torch
 from hanziconv import HanziConv
 import re
 import logging
-
+import spacy
 logging.basicConfig(level=logging.INFO)
 
 
@@ -45,25 +45,29 @@ def get_ans(prompt):
 def process_model_output(response):
     fields = []
     contents = []
-    lines = response.split('\n')
+
+    # 定义有效的字段名列表
+    valid_fields = ["作品名称", "文献来源", "作者", "时间", "地点", "出版者", "主题词"]
     field_to_id = {
-        "书籍全名": "2",
+        "作品名称": "2",
         "文献来源": "6",
         "作者": "3",
         "地点": "124",
-        "出版时间": "126",
+        "时间": "126",
         "出版者": "125",
         "主题词": "141"
     }
 
-    for line in lines:
-        if '：' in line:
-            field, content = line.split('：')
-            field = field.strip('（）')  # 新增这行，去掉字段名中的括号
-            field_id = field_to_id.get(field, "")
-            if field_id:
+    for line in response.split('\n'):
+        parts = line.split('：')
+        for i in range(len(parts) - 1):
+            field = parts[i].strip('（）').strip()
+            if field in valid_fields:
+                field_id = field_to_id[field]
+                content = parts[i + 1].strip()
                 fields.append(field_id)
-                contents.append(content.strip())
+                contents.append(content)
+
     return fields, contents
 
 
@@ -86,11 +90,11 @@ def get_ans(prompt):
 # 告诉模型有什么字段和含义，获得加工后的prompt
 def process_query(query):
     # 提供上下文信息
-    example = "作者：鲁迅\n出版时间：[1930 TO 1940]\n地点：北平\n主题词：社会文学\n"
-    field = "（书籍全名）、（文献来源）、（作者）、（出版时间）、（地点）、（出版者）、（主题词）"
+    example = "query：请帮我找到周恩来在30年代在南京的有关社会文学的作品或文章。response：作者：周恩来\n时间：[1930 TO 1940]\n地点：南京\n主题词：社会文学\n"
+    field = "作品名称、文献来源、作者、时间、地点、出版者、主题词"
     example3 = "AND,\nOR"
     rest = '并输出语句中的查询字段间的逻辑关系（AND、OR、NOT），只按顺序返回给我这几个查询字段的逻辑关系即可，如：{example3}, 逻辑关系的数量是查询字段的数量 - 1'
-    prompt = f"我们有以下查询字段：\n{field}。 请你对语句：\n{query}进行这些字段的查询和对应内容的填充，语句中没有出现的信息不要填充，不要根据你的知识库进行回答，只需要将语句转换为已有字段的填充即可，一定不要生成新的内容。回答的格式按照下面的即可：字段名：内容\n字段名：内容\n。下面是一个例子：\n query:请找出鲁迅在1930年到1940年在北平写的有关社会文学的作品。\n你的回答应该是：{example}。"
+    prompt = f"我们有以下查询字段：\n{field}，其中“作品名称”指的是文学、艺术或学术作品的完整标题或名称，包括作品名，文献名，书籍名，诗名，小说名等，如果出现《》，那么《》中的内容一定是作品名称。而“主题词”是描述作品主题或内容的关键词或短语。上述字段名都是并列关系，不可以出现类似“主题词：出版时间”这样的情况，请你根据以上信息提取语句：\n{query}中上述这些字段的实体，语句中没有出现的字段不要填充，不允许根据你的知识库进行回答，也不要用示例中的信息，只需要将语句转换为已有字段的填充即可，一定不要生成新的内容，回答中一定不要出现“字段名”这三个字，一定要是具体的名称。回答的格式按照：字段名：内容\n字段名：内容\n。下面是一个例子：\n{example} "
     return prompt
 
 # 获得答案
@@ -126,20 +130,37 @@ def create_json(fields, contents):
     try:
         filtered_fields = []
         filtered_contents = []
+        filtered_combines = []
+        start = end = None
+
         for field, content in zip(fields, contents):
             content = content.strip()
             content = convert_decade(content)  # 转换时间字段
-            if content and content.lower() not in ("无", "NULL", "null", "不填充"):
-                filtered_fields.append(field.strip())
-                filtered_contents.append(content)
-        combines = ["OR"] * (len(filtered_fields) - 1)
+            if content and content.lower() not in ("无", "NULL", "null", "不填充", "未知", "未提及", "Unknown", "None", "空", "Undefined", "不确定"):
+                if content.startswith('[') and ' TO ' in content and content.endswith(']'):
+                    # 解析时间范围
+                    start, end = content.strip('[]').split(' TO ')
+                    start, end = int(start), int(end)
+                else:
+                    if filtered_fields and field in ("126", "3", "124") and filtered_fields[-1] in ("126", "3", "124"):
+                        filtered_combines.append("AND")
+                    elif filtered_fields:
+                        filtered_combines.append("OR")
+                    filtered_fields.append(field.strip())
+                    filtered_contents.append(content)
+
         types = ["0"] * len(filtered_fields)
         result = {
             "fields": filtered_fields,
             "contents": filtered_contents,
-            "combines": combines,
+            "combines": filtered_combines,
             "types": types
         }
+
+        if start is not None and end is not None:
+            result["start"] = start
+            result["end"] = end
+
         return result
     except Exception as e:
         logging.error(f"An error occurred while creating JSON: {str(e)}")
@@ -150,13 +171,14 @@ def create_json(fields, contents):
 def final_result(query):
     processed_query = process_query(query)
     query_field = get_query_field(processed_query)
+    print(query_field)
     fields, contents = process_model_output(query_field)
     result_json = create_json(fields, contents)
     result_json_str = json.dumps(result_json, indent=4, ensure_ascii=False)
     return result_json_str
 
-# query = '和妇女相关的近代文献'
-# print(final_result(query))
+query = '周树人在40年代写的作品'
+print(final_result(query))
 
 
 
